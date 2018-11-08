fa026b1cf53a1067524dc732eb0dbba2b1c41fb7
Brian Grant <brian.grant@urbanairship.com>
2018-11-07 20:24:34 -0800

    add readme

--- load_udf.js
+++ load_udf.js
@@ -1,9 +1,11 @@
+## creates a random uuid for operation
 function uuidv4() {
   return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
     (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
   )
 }
 
+## converts seconds of day to time
 function convertSeconds(sec) {
   var hrs = Math.floor(sec / 3600);
   var min = Math.floor((sec - (hrs * 3600)) / 60);
@@ -16,6 +18,7 @@ function convertSeconds(sec) {
   return result;
 }
 
+## converts the date string to date format recognized by bigquery
 function makeDate(datestring) {
   var parts = datestring.split(':');
   var dateparts = [parts[0].substring(5, 9), parts[0].substring(2, 5), parts[0].substring(0, 2)]
@@ -23,36 +26,38 @@ function makeDate(datestring) {
   return Date.parse(date);
 }
 
+## takes the inported csv line, splits values on commas, performs transforms then returns json object
 function transform(line) {
-  var values = line.split(',');
+  var values = line.split(','); ## splits comma separated text into an array
 
-  var obj = new Object();
-  obj.id = uuidv4();
-  obj.service_date = makeDate(values[0]);
-  obj.vehicle_number = values[1];
-  obj.leave_time = convertSeconds(values[2]);
-  obj.train = values[3];
-  obj.route_number = values[5];
-  obj.direction = values[6];
-  obj.service_key = values[7];
-  obj.trip_number = values[8];
-  obj.stop_time = convertSeconds(values[9]);
-  obj.arrive_time = convertSeconds(values[10]);
-  obj.dwell = values[11];
-  obj.location_id = values[12];
-  obj.door = values[13]
-  obj.lift = values[14]
-  obj.ons = values[15]
-  obj.offs = values[16]
-  obj.maximum_speed = values[17];
-  obj.train_mileage = values[18];
-  obj.pattern_distance = values[19];
-  obj.location_distance = values[20];
-  obj.x_coordinate = values[22]
-  obj.y_coordinate = values[23];
+  var stopEvent = new Object(); ## creates the stopEvent object
+
+  stopeEvent.id = uuidv4(); ## random uuid
+  stopeEvent.service_date = makeDate(values[0]);
+  stopeEvent.vehicle_number = values[1];
+  stopeEvent.leave_time = convertSeconds(values[2]);
+  stopeEvent.train = values[3];
+  stopeEvent.route_number = values[5];
+  stopeEvent.direction = values[6];
+  stopeEvent.service_key = values[7];
+  stopeEvent.trip_number = values[8];
+  stopeEvent.stop_time = convertSeconds(values[9]);
+  stopeEvent.arrive_time = convertSeconds(values[10]);
+  stopeEvent.dwell = values[11];
+  stopeEvent.location_id = values[12];
+  stopeEvent.door = values[13]
+  stopeEvent.lift = values[14]
+  stopeEvent.ons = values[15]
+  stopeEvent.offs = values[16]
+  stopeEvent.maximum_speed = values[17];
+  stopeEvent.train_mileage = values[18];
+  stopeEvent.pattern_distance = values[19];
+  stopeEvent.location_distance = values[20];
+  stopeEvent.x_coordinate = values[22]
+  stopeEvent.y_coordinate = values[23];
 
 
-  var jsonString = JSON.stringify(obj);
+  var jsonStopEvent = JSON.stringify(stopEvent);
 
-  return jsonString;
+  return jsonStopEvent;
 }
